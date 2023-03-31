a73c931f5ce9e384dd574b1cab9288e2f609ed35
Raudhatul02 <129485653+Raudhatul02@users.noreply.github.com>
2023-03-31 13:09:21 +0700

    Add files via upload

---
+++ lib/data/news_data.dart
@@ -0,0 +1,55 @@
+import 'package:pertemuan_v/models/news.dart';
+
+List<News> hotNews = [
+  News(
+    id: 6,
+    title: "Indonesia Gagal Jadi Tuan Rumah Piala Dunia U-20 2023",
+    description:
+        "TIMNAS Indonesia U-20 gagal mentas di Piala Dunia U-20 2023. Striker utama Garuda Nusantara (julukan Timnas Indonesia U-20), Hokky Caraka mengatakan kegagalan itu bisa dijadikan pembelajaran berharga bagi seluruh pihak.",
+    image:
+        "https://img.okezone.com/content/2023/03/30/51/2789927/timnas-indonesia-u-20-gagal-mentas-di-piala-dunia-u-20-2023-hokky-caraka-beri-tanggapan-menohok-XtQmoZVo4K.jpg",
+  ),
+];
+
+List<News> newsData = [
+  News(
+    id: 1,
+    title: "Pencabutan status tuan rumah",
+    description:
+        "Pencabutan status tuan rumah Indonesia itu diduga buntut dari penolakan terhadap salah satu peserta turnamen Piala Dunia U-20 2023, Timnas Israel U-20. ",
+    image:
+        "https://img.okezone.com/okz/500/library/images/2023/03/30/59aihpeoob12e1hf1uxj_14235.jpeg",
+  ),
+  News(
+    id: 2,
+    title: "Kronologi Indonesia Gagal Jadi Tuan Rumah Piala Dunia U-20 2023 dan Daftar Tokoh yang Menentang",
+    description:
+        "FIFA secara resmi telah membatalkan status Indonesia sebagai tuan rumah Piala Dunia U-20 2023. Informasi ini diumumkan pada Rabu malam WIB, 30 Maret, melalui situs resmi mereka. ",
+    image:
+        "https://statik.tempo.co/data/2022/11/30/id_1161582/1161582_720.jpg",
+  ),
+  News(
+    id: 3,
+    title: "Indonesia Jadi Negara Ketiga yang Gagal Jadi Tuan Rumah Piala Dunia U-20 Akibat Situasi Politik",
+    description:
+        "FIFA mengatakan mencoret Indonesia karena situasi terkini, tetapi tidak menjelaskan secara detail. Kabar ini tentu sangat memilukan karena menjadikan Indonesia sebagai negara keempat yang gagal menggelar Piala Dunia U-20. Indonesia juga jadi negara ketiga yang gagal karena situasi politik.",
+    image:
+        "https://cdns.klimg.com/bola.net/library/upload/21/2022/08/645x430/piala-dunia-u-20-1_e7521c9.jpg",
+  ),
+  News(
+    id: 4,
+    title: "Maskot Piala Dunia U-20",
+    description:
+        "Maskot Piala Dunia U-20 2023 Indonesia bernama Bacuya berpose dalam acara 100 Hari Menuju Piala Dunia U-20 Indonesia 2023 di Stadion Utama Gelora Bung Karno, Jakarta, Kamis (9/2/2023). Kementerian Pemuda dan Olahraga bersama Persatuan Sepak Bola Seluruh Indonesia (PSSI) resmi menghitung mundur 100 hari menuju perhelatan Piala Dunia U-20 2023 yang akan berlangsung di Indonesia pada 20 Mei-11 Juni 2023.",
+    image:
+        "https://asset.kompas.com/crops/6_MzrgTPgdzfGsD_LrelRIlDTM0=/0x0:1996x1331/750x500/data/photo/2023/03/24/641d61eaa610d.jpg",
+  ),
+  News(
+    id: 5,
+    title: "Kronologi & Timeline RI Batal Jadi Tuan Rumah Piala Dunia U20",
+    description:
+        "Piala Dunia U-20 2023 Indonesia resmi dibatalkan oleh FIFA. Kabar dibatalkannya status Indonesia sebagai tuan rumah diungkapkan FIFA melalui laman resminya, Rabu (29/3/2023) malam",
+    image:
+        "https://akcdn.detik.net.id/visual/2018/01/14/ec3737ff-f11e-46f8-a1f4-ebae91c7e85b_169.jpeg?w=715&q=90",
+  ),
+];
